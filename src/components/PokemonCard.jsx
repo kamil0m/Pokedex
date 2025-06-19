d8e65aafbbd4babbe3546508a4169ce8af8c85f7
Kamil Markowski <kamil0markowski@gmail.com>
2025-06-19 19:25:29 +0200

    pokemon selection added

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+export default function PokemonCard({selectedPokemon}) {
+    console.log(selectedPokemon);
+
+    const [pokemon, setPokemon] = useState(selectedPokemon);
+    
+
+    useEffect(() => {
+        setPokemon(selectedPokemon);
+    },[selectedPokemon]);
+
+  return (
+    <div>
+        {pokemon && (
+            <div>
+                <h2>{pokemon.name}</h2>
+                <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                <p>Height: {pokemon.height}</p>
+                <p>Weight: {pokemon.weight}</p>
+            </div>
+        )}
+    </div>
+  )
+}
--- src/components/PokemonsList.jsx
+++ src/components/PokemonsList.jsx
@@ -1,32 +1,58 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
+import PokemonCard from "./PokemonCard";
 
 export default function PokemonsList() {
-  const [data, setData] = useState(undefined);
-  const [error, setError] = useState(null);
+    const [data, setData] = useState(undefined);
+    const [selectedPokemon, setSelectedPokemon] = useState(null);
+    const [error, setError] = useState(null);
 
-  useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
-      .then((response) => response.json())
-      .then((data) => setData(data));
-  }, []);
+    const pokemonImageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
 
-  //   console.log(data.results);
-  //    https://pokeapi.co/api/v2/pokemon/ditto
+    useEffect(() => {
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0")
+        .then((response) => response.json())
+        .then((data) => setData(data));
+    }, []);
+
+// Test if selectedPokemon state updates:
+//   useEffect(() => {
+//     console.log("Selected Pokemon's object in state:", selectedPokemon);
+//   }, [selectedPokemon]);
+  
+    function showPokemon(index) {
+        console.log("Selected Pokemon's index:", index);
+        // Fetch the details of the selected Pokemon
+        fetch(`https://pokeapi.co/api/v2/pokemon/${index}`)
+        .then((response) => response.json())
+        .then((data) => {
+            setSelectedPokemon(data);
+        })
+        .catch((error) => setError(error));
+    }
 
   return (
-    <div>
-      <p>Pokemons' list :</p>
-      {data && data.results && (
-        <ul>
-          {data.results.map((pokemon, index) => (
-            <div key={index}>
-              <li>{pokemon.name}</li>
-              <img href={pokemon.url} alt={pokemon.name} />
-              <button>Show more details</button>
-            </div>
-          ))}
-        </ul>
-      )}
-    </div>
+    <>
+        <div>
+            <p>Pokemons' list :</p>
+            {data && data.results && (
+                <div>
+                    <ul>
+                        {data.results.map((pokemon, index) => (
+                            <li key={index} style={{ 
+                                display: "flex",
+                                flexDirection: "row",
+                                alignItems: "center",
+                            }}>
+                                <span>{pokemon.name}</span>
+                                <img src={`${pokemonImageUrl}${index+1}.png`} alt={pokemon.name} />
+                                <button onClick={() => showPokemon(index+1)}>Show more details</button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
+        {selectedPokemon && <PokemonCard selectedPokemon={selectedPokemon} />}
+    </>
   );
 }
