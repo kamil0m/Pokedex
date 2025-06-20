1fa8077c4665a36d843fe2a1a2e2e29270a2e774
Kamil Markowski <kamil0markowski@gmail.com>
2025-06-20 08:28:54 +0200

    additional info added to PokemonCard display

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function PokemonCard({selectedPokemon}) {
     const [pokemon, setPokemon] = useState(selectedPokemon);
@@ -7,6 +8,17 @@ export default function PokemonCard({selectedPokemon}) {
         setPokemon(selectedPokemon);
     },[selectedPokemon]);
 
+    // async function fetchTypeImage(typeUrl) {
+    //     // Fetch the type image from the provided URL
+    //     const imageUrl = await axios.get(typeUrl)
+    //         .then(response => return response.data.sprites['generation-iii']['firered-leafgreen'].name_icon)
+    //         .catch(error => {
+    //             console.error("Error fetching type image:", error);
+    //             return "";
+    //         });
+    //     return imageUrl;
+    // }
+
   return (
     <div className="h-full">
         {pokemon && (
@@ -14,9 +26,22 @@ export default function PokemonCard({selectedPokemon}) {
                 <h2 className="text-3xl">{pokemon.name}</h2>
                 <div className="flex flex-row align-center h-full">
                     <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                    {/* {pokemon.types.map((type) => {
+                        <img 
+                            key={type.type.name} 
+                            src={fetchTypeImage(type.type.url)} 
+                            alt={type.type.name} 
+                            className="h-8 w-8"
+                        />
+                        
+                    })} */}
                     <div className="flex flex-col">
                         <p>Height: {pokemon.height}</p>
                         <p>Weight: {pokemon.weight}</p>
+                        {pokemon.stats.map((stat, index) => (
+                            <p key={index}>{stat.stat.name}: {stat.base_stat}</p>
+                        ))}
+                        <p>Types: {pokemon.types.map((type) => type.type.name).join(', ')}</p>
                     </div>
                 </div>
             </div>
